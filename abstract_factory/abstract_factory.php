e50fb856d42591ebead7581b277779f5e4725221
yongliu <a3831524@126.com>
2017-10-12 19:37:44 +0800

    抽象工厂

--- abstract_factory/abstract_factory.php
+++ abstract_factory/abstract_factory.php
@@ -2,11 +2,18 @@
 
 class Button{}
 class Border{}
+
 class MacButton extends Button{}
 class WinButton extends Button{}
+
 class MacBorder extends Border{}
 class WinBorder extends Border{}
 
+/***
+ * 接口里面定义 创建类对象的方法
+ * 由工厂类去实现抽象接口
+ * Interface AbstractFactory
+ */
 interface AbstractFactory {
     public function CreateButton();
     public function CreateBorder();
